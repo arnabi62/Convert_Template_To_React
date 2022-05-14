5c5c4cb2e4d4d1e198de59da25ec8c50f03beb38
Arnabi Mitra <arnabimitra100@gmail.com>
2022-05-14 21:03:32 +0530

    use of states

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
-import React from "react"
-import Footer from "./Footer";
-import Jumpborton from "./Jumpborton";
-import NavBar from "./NavBar";
-import AppSection from "./AppSection"
+import React, {useState} from "react"
+import "./App.css"
+
 
 const App = () =>
 {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-     <NavBar/>
-     <Jumpborton/>
-      <AppSection/> 
-     <Footer/>
+    <h2>Current value of count : {count}</h2>
+    <br/>
+    <button onClick ={()=> setCount(0)}>Reset</button>
+    <button onClick = {()=> count >=10 ?"":setCount(count+1)}>Increase</button>
+    <button onClick = {()=> count<=0?"":setCount(count-1)}>Decrease</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import ReactDOM from 'react-dom';
 
 
  import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//reportWebVitals();
+
